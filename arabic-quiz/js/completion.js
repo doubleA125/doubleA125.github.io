e05e14e0aa7707bb15ef4208a66057184d39f871
doubleA125 <boody2012atef@gmail.com>
2025-06-04 10:45:04 +0300

    Delete arabic-quiz directory

--- arabic-quiz/js/completion.js
+++
@@ -1,6 +0,0 @@
-// completion.js
-document.getElementById('leaderboardButton').addEventListener('click', viewLeaderboard);
-
-async function viewLeaderboard() {
-    window.location.href = "leaderboard.html"; // Redirect to leaderboard page
-}
